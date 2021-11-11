15a0e9af1c1b50d04c197c8b3f0d1653d8b11aff
keszinaj <kubajaniszek123@gmail.com>
2021-11-11 13:15:03 +0100

    add comments to sudoku solver

--- sudoku_solver/main.py
+++ sudoku_solver/main.py
@@ -1,29 +1,32 @@
+# made by keszinaj
+'''
+    It's simple sudoku solver.
+'''
 import math
-# hard input
-sudoku_input = ([8, 0, 0, 0, 0, 0, 0, 0, 0],
-               [0, 0, 3, 6, 0, 0, 0, 0, 0],
-               [0, 7, 0, 0, 9, 0, 2, 0, 0],
-               [0, 5, 0, 0, 0, 7, 0, 0, 0],
-               [0, 0, 0, 0, 4, 5, 7, 0, 0],
-               [0, 0, 0, 1, 0, 0, 0, 3, 0],
-               [0, 0, 1, 0, 0, 0, 0, 6, 8],
-               [0, 0, 8, 5, 0, 0, 0, 1, 0],
-               [0, 9, 0, 0, 0, 0, 4, 0, 0])
+from testy import sudoku_input_hard, sudoku_input_wrong, invalidBoard
+
+'''
+    Row can only contain each number from 1 to 9 once. This function check it.
+'''
 def valid_row(n, sudoku_arr, row):
     return not n in sudoku_arr[row]
 
-# take number in row
+'''
+    Collumn can only contain each number from 1 to 9 once. This functions check it.
+'''
 def column_as_array(sudoku_arr, num):
     r = []
     for i in range(9):
         r.append(sudoku_arr[i][num])
     return r
-
 def valid_column(n, sudoku_arr, column):
     return not n in column_as_array(sudoku_arr, column)
 
+'''
+    Square can only contain each number from 1 to 9 once. This functions check it.
+'''
 def square_as_arr(sudoku_arr, r, c):
-    c = math.floor(c / 3) # sprawdz czzy nie ma błedu typu chyba nie
+    c = math.floor(c / 3) 
     r = math.floor( r / 3)
     arr = []
     for i in range(3):
@@ -32,7 +35,11 @@ def square_as_arr(sudoku_arr, r, c):
     return arr
 def valid_square(n, sudoku_arr, row, column):
     return not n in square_as_arr(sudoku_arr, row, column)
-def sol(sudoku, row, column):
+
+'''
+    Backtracking algorithm which tries to complete sudoku.
+'''
+def backtracking_algorithm(sudoku, row, column):
     if row == 9:
         return True
     if sudoku[row][column] == 0:
@@ -43,7 +50,7 @@ def sol(sudoku, row, column):
                     if column == 9:
                         column = 0
                         row += 1
-                    if sol(sudoku, row, column):
+                    if backtracking_algorithm(sudoku, row, column):
                         return True
                     if column == 0:
                         row -= 1
@@ -61,12 +68,32 @@ def sol(sudoku, row, column):
         if column == 9:
             column = 0
             row += 1
-        return sol(sudoku, row, column)
- 
-def solver(sudoku):
-    s = sol(sudoku, 0, 0)
-    print(sudoku)
-    return s
+        return backtracking_algorithm(sudoku, row, column)
 
-print(solver(sudoku_input))
-   
+'''
+    Pretty print sudoku in console.
+'''
+def prettyprint(sudoku):
+    if sudoku is None:
+        print("Solution doesn't exist.")
+    else:
+        print("+---" * 9 + "+")
+        for row in sudoku:
+            print(("|" + " {} |" * 9 + "\n" + "+---" * 9 + "+").format(*[i for i in row]))
+        
+'''
+    main function 
+'''
+def sudoku_solver(sudoku):
+    s = backtracking_algorithm(sudoku, 0, 0)
+    if s:
+      return sudoku
+    else:
+        return None
+
+    
+if __name__ == "__main__":
+    prettyprint(sudoku_solver(sudoku_input_hard))
+    prettyprint(sudoku_solver(sudoku_input_wrong))
+    prettyprint(sudoku_solver(invalidBoard))
+      
