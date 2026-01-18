7fabe3afbf7fb81b30c9c6e430a1aee156d6517d
keszinaj <kubajaniszek123@gmail.com>
2026-01-18 21:13:40 +0100

    working for 3rd 4th degree polynomial

--- shamir_secret_sharing/main.py
+++ shamir_secret_sharing/main.py
@@ -4,7 +4,7 @@ secret_as_numer=''.join(str(x) for x in list(map(lambda x: ord(x), list(secret))
 
 #define minimal number of shared secrets nessesary to put
 
-m=4
+m=8
 
 # stopien wielomianu polynomial
 degree = m-1
@@ -19,22 +19,29 @@ ss = 5
 import random
 def random_polynomial(k):
     list_of_A = []
-    for q in range(k+1):
+    for _ in range(k+1):
         list_of_A.append(random.randint(1,10000))
     print(list_of_A)
     return list_of_A
 
-#a = random_polynomial(degree)
-
+a = random_polynomial(degree)
+print(a)
 
 def calculate_random_point_in_polynomial(list_of_A, k):
     x=random.randint(1,10000)
     y=0
     for i in range(k+1):
         y+=pow(x,i)*list_of_A[i]
-    return y
-
-
+    return (x,y)
+xxx = []
+yyy = []
+for _ in range(degree+1):
+    point = calculate_random_point_in_polynomial(a, degree)
+    xxx.append(point[0])
+    yyy.append(point[1])
+    print("Point:", point)
+print("x:", xxx)
+print("y:", yyy)
 
 
 # zastanawialem sie jak pomnozyc dwa wielomiany
@@ -72,46 +79,62 @@ def determining_an(a, x):
     arr = [0 for _ in range(len(a))]
     arr[0] = a[0]
     for i in range(len(x)-1):
-        temp_sol = [-1*x[i], 1] #x -x_n
+        temp_sol = [-1*x[i], 1] # x - x_n
         for j in range(i):
            temp_sol = polynomial_multiplation(temp_sol, [-1*x[j], 1])
-        print(temp_sol)
+        #print(temp_sol)
         for o in range(len(temp_sol)):
             arr[o] += temp_sol[o] *a[i+1]
-        print(arr)
+        #print(arr)
     return(arr)    
 
 
 #test determining_an()
 #determining_an([1, 1, 1], [1, 2,100])
 #determining_an([1, 1, 1, 1], [1, 2, 3, 4])
-determining_an([4, 5, 1], [1, 2, 3])
+#determining_an([4, 5, 1], [1, 2, 3])
+
+# no niby dziala ale to trzeba jeszcze chyba wiecej przykladow porobic do testow
+# no i to wszystko trzeba teraz polaczyc
 
 
 def lagrange_polynomial(x_arr, y_arr):
     number_of_points = len(x_arr)
-    arr = [[0 for _ in range(number_of_points)] for _ in range(number_of_points)]
-    print(arr)
-    #wypelnij skos
+    arr = [[0.0 for _ in range(number_of_points)] for _ in range(number_of_points)]
+    # Wypełnij pierwszą kolumnę wartościami y
     for i in range(number_of_points):
         arr[i][i] = y_arr[i]
-    print(arr)
+    #print(arr)
     # wypelniamy nastepne skosy
     for j in range(1, number_of_points):
         for i in range(number_of_points-1):
-            print("i:", i, "j:", j)
+            #print("i:", i, "j:", j)
             numerator = arr[i+1][j]-arr[i][j-1]
             denominator = x_arr[j] - x_arr[i]
             sol= numerator/denominator
-            print("numerator",numerator, "denominator:",denominator)
+            #print("numerator",numerator, "denominator:",denominator)
             arr[i][j]=sol
             j+=1
             if j==len(x_arr):
                 break
-    print(arr)
+    #print(arr)
+    # bo tam tylko sa wyniki zobacz na screenshocie
+    return arr[0]
+
    
 
 
 
 #test Lagrange_polynomia
 #lagrange_polynomial([1, 2, 3], [4, 9, 16])
+
+
+# test wszystkiego razem
+ta = lagrange_polynomial(xxx, yyy)
+print(determining_an(ta, xxx))
+
+#ta= lagrange_polynomial([1,2,3], [4,5,1])
+#print(determining_an(ta, [1,2,3])) # powinno dać [-2.0, 8.5, -2.5]
+
+#ta= lagrange_polynomial([0,1,2,3], [-1,-1,5,11])
+#print(determining_an(ta, [-1,0,2,3]))
