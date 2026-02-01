3581a8c7b408e2596e72e30f57ab4462c9faccb8
keszinaj <kubajaniszek123@gmail.com>
2026-02-01 19:56:44 +0100

    cleaner code

--- shamir_secret_sharing/shamir_secret_sharing.py
+++ shamir_secret_sharing/shamir_secret_sharing.py
@@ -1,25 +1,5 @@
+# made by keszinaj
 import hashlib
-
-# TODO: kod dziala ale trzeba go posprzatac i dodac komentarze
-# define full secret
-
-# global configuration 
-#define minimal number of shared secrets nessesary to put
-
-m=2
-
-# stopien wielomianu polynomial
-degree = m-1
-
-# secrets to genereate(must be bigger or equal m)
-
-ss = 5
-
-
-secret="alamakota"
-secret_as_numer=''.join(str(x) for x in list(map(lambda x: ord(x), list(secret))))
-
-
 
 # generate random polynomial
 # f(0) = secret
@@ -32,37 +12,25 @@ def random_polynomial(k):
     print(list_of_A)
     return list_of_A
 
-#a = random_polynomial(degree)
-#print(a)
-
+#  get random point in polynomial for secret
 def calculate_random_point_in_polynomial(list_of_A, k):
     x=random.randint(1, 10000)
     y=0
     for i in range(k+1):
         y+=pow(x,i)*list_of_A[i]
     return (x,y)
-#xxx = []
-#yyy = []
-#for _ in range(degree+1):
-#    point = calculate_random_point_in_polynomial(a, degree)
-#    xxx.append(point[0])
-#    yyy.append(point[1])
-#    print("Point:", point)
-#print("x:", xxx)
-#print("y:", yyy)
 
+# I was thiking how to multiply two polynomials.
+# The idea that came to my mind was to represent them as arrays.
+# For example, 10x^3+7x^2-2x+67 can be represented as [67, -2, 7, 10].
+# then such an operation (2x + 7)*(6x+10) is the multiplication of [7,2,0] * [10,6,0] and then each element from the array on the left is multiplied
+# by the element on the right and inserted into the array at i+1, unless we are multiplying a free element, in which case we leave it in the free space
 
-# zastanawialem sie jak pomnozyc dwa wielomiany
-# pomysl ktory mi przyszedl do glowy to przedstawic je jako tablice
-# np. 10x^3+7x^2-2x+67 mozna przedstawic jako [67, -2, 7, 10]
-# wtedy takie działanie (2x + 7)*(6x+10) to mnożenie [7,2,0] * [10,6,0] i wtedy kazdy element z tej tablicy z lewej mnozymy
-# przez element z prawej i wstawiamy to tablei w i+1 chyba ze mnożymy element wolny wtedy zostawiamy to na wolnym miejscu
-
-#UWAGA
-# na chwile dla latwosci przyjmijmy ze len(w1)>len(w2)
-# no i zakladamy ze w2 zawsze bedzie drugiego stopnia wtedy ten stopien wilomianu moze sie zemienic max 1 do przedy
-# w ogolnym przypadku byloby wiecej edge casow, ale no nie oplaca sie teog implementowac
-# w moim przykladzie bede mnozyl od lewej do prawej
+# Note:
+# for simplicity, let us assume that len(w1)>len (w2)
+# and we assume that w2 will always be of the second degree, then this degree of the polynomial can change by a maximum of 1 forward
+# in general, there would be more edge cases, but it is not worth implementing them
+# in my example, I will multiply from left to right
 def polynomial_multiplation(w1, w2):
     if len(w2) > len(w1):
         print("ERROR")
@@ -78,11 +46,9 @@ def polynomial_multiplation(w1, w2):
                 solution[i+j]+=w1[i]*w2[j]
     if solution[w1_size] == 0:
         solution.pop() 
-   # print(solution)
     return solution
 
-#print(polynomial_multiplation([2, -3, 1], [-3, 1]))
-
+# determine a_n coefficients of polynomial from Newton Polynomial form
 def determining_an(a, x):
     arr = [0 for _ in range(len(a))]
     arr[0] = a[0]
@@ -90,32 +56,20 @@ def determining_an(a, x):
         temp_sol = [-1*x[i], 1] # x - x_n
         for j in range(i):
            temp_sol = polynomial_multiplation(temp_sol, [-1*x[j], 1])
-        #print(temp_sol)
         for o in range(len(temp_sol)):
             arr[o] += temp_sol[o] *a[i+1]
-        #print(arr)
-    print(arr)
     arr = [int(num) for num in arr]
-    print(arr)
     return(arr)    
 
-
-#test determining_an()
-#determining_an([1, 1, 1], [1, 2,100])
-#determining_an([1, 1, 1, 1], [1, 2, 3, 4])
-#ogh=determining_an([4, 5, 1], [1, 2, 3])
-#print('ogh', ogh)
-# no niby dziala ale to trzeba jeszcze chyba wiecej przykladow porobic do testow
-# no i to wszystko trzeba teraz polaczyc
-
-
+# Lagrange interpolation to find coefficients of polynomial it uses Newton's divided differences method
+# https://en.wikipedia.org/wiki/Newton_polynomial
 def lagrange_polynomial(x_arr, y_arr):
     number_of_points = len(x_arr)
     arr = [[0.0 for _ in range(number_of_points)] for _ in range(number_of_points)]
-    # Wypełnij pierwszą kolumnę wartościami y
+    # Fullfill first column with y values
     for i in range(number_of_points):
         arr[i][0] = y_arr[i]
-    # Wypełnij pozostałe kolumny różnicami dzielonymi
+    # Fulfill rest colums różnicami dzielonymi
     for j in range(1, number_of_points):
         for i in range(number_of_points - j):
             numerator = arr[i+1][j-1] - arr[i][j-1]
@@ -125,39 +79,6 @@ def lagrange_polynomial(x_arr, y_arr):
     return arr[0]
 
    
-
-
-
-#test Lagrange_polynomia
-#lagrange_polynomial([1, 2, 3], [4, 9, 16])
-
-
-# test wszystkiego razem
-# okej to dobrze dziala dla wielomianow stopnia 1, 2, 3 dla 4 juz sie psuje
-a = random_polynomial(degree)
-secret_key = "".join(str(i) for i in a)
-#to meke it 32 bytes as chacha20 require
-res = hashlib.md5(secret_key.encode())
-secret_key=res.hexdigest()
-
-print(secret_key)
-print(len(secret_key))  # 32
-xxx = []
-yyy = []
-for _ in range(degree+1):
-    point = calculate_random_point_in_polynomial(a, degree)
-    xxx.append(point[0])
-    yyy.append(point[1])
-    print("Point:", point)
-
-
-
-#ta = lagrange_polynomial([9262, 8210, 8293], [90569871, 80283415, 81094989])
-#print(determining_an(ta, xxx))
-
-#key format of the chacha20 will be
-# n $ nonce $ x_coordinate $ y_coordinate
-
 
 # Math thoughts
 # czy to ma znaczenie ze moje wspolczynniki sa zawsze liczbami calkowitymi? czy 
---
+++ shamir_secret_sharing/test_operation_on_polynomial.py
@@ -0,0 +1,5 @@
+#made by keszinaj
+# this file contains test cases for polynomial calulaction that allows to 
+# implement shamir secret sharing scheme
+
+import shamir_secret_sharing
